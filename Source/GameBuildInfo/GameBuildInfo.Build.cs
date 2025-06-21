caf6584a8483ecb0e7d6fae1a4b8d9c802a14f87
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2025-06-21 00:38:00 -0400

    Fix use when built from engine & add loggging
    Previously the working directory when building from Visual Studio might be the local engine version instead of the project. Now the code walks the module path to look for the local project location to use as working directory to run the perforce commands.
    
    Added logging to better diagnose issues.

--- Source/GameBuildInfo/GameBuildInfo.Build.cs
+++ Source/GameBuildInfo/GameBuildInfo.Build.cs
@@ -3,6 +3,7 @@
 using System.Diagnostics;
 using System;
 using UnrealBuildTool;
+using System.IO;
 
 public class GameBuildInfo : ModuleRules
 {
@@ -13,7 +14,7 @@ public class GameBuildInfo : ModuleRules
         PublicDependencyModuleNames.AddRange(new string[]
             {
                 "Core",
-			});
+            });
 
 
         PrivateDependencyModuleNames.AddRange(new string[]
@@ -34,31 +35,74 @@ public class GameBuildInfo : ModuleRules
         // Build Constant: Perforce changelist
         {
             string p4CL = "-1";
+
             try
             {
+                System.Console.WriteLine("GameBuildInfo.Build.cs: Looking for current Perforce changelist.");
+
+
                 var p4 = new Process();
                 p4.StartInfo.FileName = "p4";
                 p4.StartInfo.Arguments = "changes -m1 ...#have";
                 p4.StartInfo.UseShellExecute = false;
                 p4.StartInfo.RedirectStandardOutput = true;
+                p4.StartInfo.RedirectStandardError = true;
                 p4.StartInfo.CreateNoWindow = true;
-                p4.Start();
 
+                string projectRoot = FindUProjectRoot(ModuleDirectory);
+                if (projectRoot != null)
+                {
+                    p4.StartInfo.WorkingDirectory = projectRoot;
+                    System.Console.WriteLine($"GameBuildInfo.Build.cs: Set p4 working dir to project root: {projectRoot}");
+                }
+                else
+                {
+                    System.Console.WriteLine("GameBuildInfo.Build.cs: Could not locate .uproject root. p4 may fail.");
+                }
+
+                p4.Start();
                 string output = p4.StandardOutput.ReadToEnd();
+                string errors = p4.StandardError.ReadToEnd();
                 p4.WaitForExit();
 
-                var parts = output.Split(' ');
-                if (parts.Length > 1)
+                if (p4.ExitCode == 0 && !string.IsNullOrWhiteSpace(output))
                 {
-                    p4CL = parts[1];
+                    var parts = output.Split(' ');
+                    if (parts.Length > 1)
+                    {
+                        p4CL = parts[1];
+                        System.Console.WriteLine($"GameBuildInfo.Build.cs: Found current Perforce changelist: {p4CL}.");
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("GameBuildInfo.Build.cs: couldn't find current Perforce changelist.");
+                    }
+                }
+                else
+                {
+                    System.Console.WriteLine($"GameBuildInfo.Build.cs: `p4 changes` failed. Error: {errors.Trim()}");
                 }
             }
-            catch
+            catch (Exception exception)
             {
-                System.Console.WriteLine("GameBuildInfo.Build.cs: Could not retrieve Perforce changelist. P4 may not be installed, accessible, or may not be on PATH.");
+                System.Console.WriteLine($"GameBuildInfo.Build.cs: Could not retrieve Perforce changelist. p4 may not be installed, accessible, or may not be on PATH. Exception: {exception.GetType().Name}: {exception.Message}");
             }
 
             PublicDefinitions.Add("BUILD_P4_REVISION=\"" + p4CL + "\"");
         }
+    }
+
+    // Walk up from module directory to find .uproject file
+    static string FindUProjectRoot(string startDir)
+    {
+        var directory = new DirectoryInfo(startDir);
+        while (directory != null)
+        {
+            var uprojectFiles = directory.GetFiles("*.uproject");
+            if (uprojectFiles.Length > 0)
+                return directory.FullName;
+            directory = directory.Parent;
+        }
+        return null;
     }
 }
